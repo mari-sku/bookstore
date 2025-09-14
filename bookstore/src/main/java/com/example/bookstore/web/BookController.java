21d5fa7bef632021ef3477fab6fe21e0a54a89bd
mari-sku <mari.skurnik@gmail.com>
2025-09-14 18:59:23 +0300

    new controller for booklist page and new template for it

--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -1,17 +1,35 @@
 package com.example.bookstore.web;
-
+import com.example.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 // http://localhost:8080/index
+// http://localhost:8080/booklist
 
 
 @Controller
-
-//index (etusivu) controller
 public class BookController {
 
+    // repositorion alustus ja konstruktori-injektio
+private final BookRepository repository;
+public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+    
+//index (etusivu) controller
     @GetMapping("/index")
     public String index() {
         return "index";
     }
+
+//booklist sivun controller
+    @GetMapping("/booklist")
+    public String listBooks(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist"; //booklist.html
+    }
+    
 } 
